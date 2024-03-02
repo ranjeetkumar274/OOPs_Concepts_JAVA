0016691edfcdfb3260de7398080b74bd9968f7e5
ashuuydv <147934359+Ashuuydv@users.noreply.github.com>
2024-03-02 17:22:58 +0530

    Class and Object creation in JAVA

---
+++ ClassAndObjects.java
@@ -0,0 +1,8 @@
+public class ClassAndObjects {  //creating the class
+    int x = 5;
+    public static void main(String[] args) {
+        ClassAndObjects MyObj = new ClassAndObjects();   // creation of object
+        System.out.println(MyObj.x);   //accessing the attributes
+
+    }
+}
