0866f875b24af4bb20f6e5406e9625cac6848ad5
ashuuydv <147934359+Ashuuydv@users.noreply.github.com>
2024-04-11 23:08:53 +0530

    Class in JAVA

---
+++ Class.java
@@ -0,0 +1,10 @@
+public class Class {
+    int x; //member variables
+    int y;
+    void square(){ //member methods
+        int area = x*y;
+    }
+    public static void main(String[] args) {
+
+    }
+}
